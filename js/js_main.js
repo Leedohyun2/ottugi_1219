79abd0c4fad4116774764632225f45979b3bbb9a
Leedohyun <ehqhddl123@naver.com>
2023-12-19 14:41:37 +0900

    first commit

---
+++ js/js_main.js
@@ -0,0 +1,14 @@
+// 텍스트 애니메이션
+// let mainText = document.querySelectorAll(".main_img_text1")
+// console.log(mainText);
+
+// window.addEventListener('scroll', function () {
+//    let value = window.scrollY;
+//    console.log("scrollY", value);
+
+//    if (value > 700) {
+//       mainText.style.animation = "back 1s ease-out forwards";
+//    } else {
+//       mainText.style.animation = 'slide 1s ease-out';
+//    }
+// });
